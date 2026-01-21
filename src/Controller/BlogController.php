7d03b3be54ea23f9f4bffe50154b33be67352c90
Pepe <jganavas@gmail.com>
2026-01-21 10:34:48 +0100

    oeoeoeo

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+#[Route('/')]
+final class BlogController extends AbstractController
+{
+    #[Route(name: 'app_blog_index', methods: ['GET'])]
+    public function index(PostRepository $postRepository): Response
+    {
+
+        return $this->render('blog/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_blog_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('blog/new.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{slug}', name: 'app_blog_show', methods: ['GET'])]
+    public function show(string $slug, PostRepository $postRepository): Response
+    {
+
+        $post = $postRepository->findOneBy(['slug' => $slug]);
+        if(!$post) {
+            throw $this->createNotFoundException('The product does not exist');
+        }
+
+        return $this->render('blog/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_blog_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Post $post, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('blog/edit.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_blog_delete', methods: ['POST'])]
+    public function delete(Request $request, Post $post, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($post);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
